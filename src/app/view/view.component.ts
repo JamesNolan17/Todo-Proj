e07450835d29dda21cc9b58d331aecdf746e7114
James Nolan W <vip17jamesnolan@gmail.com>
2020-08-23 00:10:14 +0800

    upload files

---
+++ src/app/view/view.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit,OnDestroy } from '@angular/core';
+import {todo_interface} from "./interface";
+import {Observable, of} from "rxjs";
+import { observable, autorun } from "mobx"
+import {select, State, Store} from "@ngrx/store";
+import {filter, map, take, toArray} from 'rxjs/operators';
+import {TodoService} from "../todo-service.service"
+import {updateItem,addItem,delItem,changeStatus} from "../actions/todo-action.actions";
+
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css'],
+  providers:[TodoService]
+})
+
+export class ViewComponent {
+  public todoArray: Array<todo_interface>;
+  public Obshow: Observable<todo_interface[]>;
+  public show: Observable<todo_interface[]>;
+  public checked: Observable<todo_interface[]>;
+  public unchecked: Observable<todo_interface[]>;
+  constructor(public store:Store<{todoReducer:todo_interface[]}>,
+              public todoService: TodoService) {
+    this.show = store.select("todoReducer");
+    this.checked = this.show.pipe(
+      map(items => items.filter(item => item.checked))
+    );
+    this.unchecked = this.show.pipe(
+      map(items => items.filter(item => !item.checked))
+    );
+  }
+  changeStatus(name){
+    this.store.dispatch(changeStatus({name: name}));
+  }
+
+  delItem(name){
+    this.store.dispatch(delItem({name: name}));
+  }
+
+}
